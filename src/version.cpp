a56d608f3a71a926c1779bc8be0a7c684569b3dd
Stipend-Developer <nicksr987@gmail.com>
2018-02-12 14:54:34 -0500

    Change Version

--- src/version.cpp
+++ src/version.cpp
@@ -33,10 +33,10 @@ const std::string CLIENT_NAME("StipendClient");
 #    include "build.h"
 #endif
 
-// git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+// git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "70100"
+#    define GIT_COMMIT_ID "70101"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
